645e161e666cafcaf3bb3a8fc8f8e6baa05527e3
Dmitrij <chebotarev@dmitry.by>
2025-06-21 20:18:28 +0500

    создал: base.html, static/css|js, подключил в settings, унаследовался от base.html в 3 созданных шаблонах

--- store/goods/models.py
+++ store/goods/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+# class Category(models.Model):
+#     name = models.CharField(max_length=50)
+#     slug = models.SlugField(unique=True, blank=True)
+
+    
+# class Weapon(models.Model):
+#     name = models.CharField(max_length=100)
+#     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+#     description = models.TextField()
+#     price = models.DecimalField(max_digits=10, decimal_places=2)
+#     stock = models.PositiveIntegerField()
+#     image = models.ImageField(upload_to='weapons/')
+#     slug = models.SlugField(unique=True, blank=True)  
+
+
